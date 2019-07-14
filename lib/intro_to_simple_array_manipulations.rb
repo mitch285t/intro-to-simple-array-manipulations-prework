40e3c4006d3a8fc00e6d0bbf0d7e4b3cd23e51fe
mitchell williams <mscottwilliams1994@gmail.com>
2019-07-14 23:57:08 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,20 @@
+def using_push(array,string)
+   countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
+  countries_in_western_africa.push("Niger")
+end
+def using_unshift(array, string)
+  neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
+  neighborhoods_in_northwest_brooklyn.unshift("Brooklyn Heights")
+end
+def using_pop(array)
+  great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
+     great_hits_of_the_nineties.pop
+end 
+def pop_with_args(array)
+  chars_in_game_of_thrones = ["Danny T.", "Tyrion Lannister", "Stable Boy", "Sandor Clegane, aka The Mountain"]
+  chars_in_game_of_thrones.pop(2)
+end
+def using_shift(array)
+  my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
+  my_favorite_cities.shift
+end
