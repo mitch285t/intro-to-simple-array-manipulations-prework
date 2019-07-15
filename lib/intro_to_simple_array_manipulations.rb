62ea730a11ee3a43a13ec25a669738d3e02066ec
mitchell williams <mscottwilliams1994@gmail.com>
2019-07-15 00:22:10 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -59,9 +59,12 @@ def using_uniq(captain_planet_and_the_planeteers)
  captain_planet_and_the_planeteers.uniq
 end
 
+private_colleges_in_newyork = ["New York University", ["Manhattan School of Music ", "Columbia University"], "The Juilliard School", "Bard College", "Cooper Union"]
 
-
-def using_flatten
+def using_flatten(private_colleges_in_newyork)
+  private_colleges_in_newyork.flatten
+  
+end
 
 
 
